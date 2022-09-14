e47b6916f3a9baae8b36de4b9e6135097f5515ff
inez.bungaria <inez.bungaria@ui.ac.id>
2022-09-14 21:19:01 +0700

    tugas 2 PBP

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    katalog_item = CatalogItem.objects.all()
+    context = {
+        "list_item" : katalog_item,
+        "nama" : "Inez Bungaria Octaviana Pardede",
+        "npm" : "2106751833"
+    }
+    return render(request, 'katalog.html', context)
