c1ef7142f1802557840bdca6d16ba2b6e22487ac
inez.bungaria <inez.bungaria@ui.ac.id>
2022-09-21 20:48:03 +0700

    update tugas 3 menambahkan test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+class WatchListTest(TestCase):
+    def test_url_html(self):
+        response=Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_xml(self):
+        response=Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_json(self):
+        response=Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
