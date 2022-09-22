9784f029291d0ee98593252e16e86a0ab49ee300
inez.bungaria <inez.bungaria@ui.ac.id>
2022-09-22 11:12:15 +0700

    update tugas 3

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,10 +6,27 @@ from django.core import serializers
 # Create your views here.
 def show_mywatchlist(request):
     watchlist = WatchList.objects.all()
+    message = ""
+    watched = 0
+    not_watched = 0
+
+    for movie in watchlist:
+        if movie.watched == True:
+            watched += 1
+        elif movie.watched == False:
+            not_watched += 1
+    if watched >= not_watched:
+        message = "Selamat, kamu sudah banyak menonton!"
+    elif not_watched >= watched:
+        message = "Wah, kamu masih sedikit menonton!"
+
+
+
     context = {
         'watchlist' : watchlist,
         'nama' : 'Inez Bungaria Octaviana Pardede',
-        'npm' : '2106751833'
+        'npm' : '2106751833',
+        'message' : message
     }
     return render(request, "mywatchlist.html", context)
 
